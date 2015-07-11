313e01011a7f9499cde856c0a5a3ce97f0d1bf12
J. Pablo Pérez Trabucco <jpereztrabucco@gmail.com>
2015-07-11 16:30:16 -0300

    set up using xcode 6.4

---
+++ testone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testone
+//
+//  Created by J. Pablo Pérez Trabucco on 7/11/15.
+//  Copyright (c) 2015 Juan Pablo Pérez Trabucco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
